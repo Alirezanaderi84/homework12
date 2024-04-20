731ab5ec5fd92d36ff4204a8cae8aebff8135741
Alirezanaderi84 <148426222+Alirezanaderi84@users.noreply.github.com>
2024-04-20 11:48:11 +0330

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        String firstname, lastname,phonenumber;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            firstname = textBox1.Text;
+            lastname = textBox2.Text;
+            phonenumber =textBox3.Text;
+            firstname = firstname.Trim();
+            lastname = lastname.Trim();
+            phonenumber=phonenumber.Trim();
+            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname) || string.IsNullOrEmpty(phonenumber)
+
+                )
+                MessageBox.Show("complete boxes");
+            else
+            {
+                string d = firstname + "," + lastname + "," + phonenumber;
+                string f = "test.csv";
+                File.AppendAllText(f, d + Environment.NewLine);
+                MessageBox.Show("information saved succesfully");
+            }
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+        }
+    }
+
